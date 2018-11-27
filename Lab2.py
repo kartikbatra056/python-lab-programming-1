35703b60efa6f94e6279866d055e613f3750f815
kartikbatra056 <43983856+kartikbatra056@users.noreply.github.com>
2018-11-27 20:27:38 +0530

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,7 @@
+d={"Name":'',"Roll no.":'',"Division":'',"department":''}
+for i in range(0,6):
+     d["Name"]=input("enter your name ")
+     d["Roll no."]=input("enter your roll no.")
+     d["Division"]=input("enter your division")
+     d["department"]=input ("enter department")
+     print(d)
